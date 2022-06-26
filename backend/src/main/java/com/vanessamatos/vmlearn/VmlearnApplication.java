4065eef369f389d154065c249a486ea9442b76d2
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-06-26 18:19:48 -0300

    Projeto criado

---
+++ backend/src/main/java/com/vanessamatos/vmlearn/VmlearnApplication.java
@@ -0,0 +1,13 @@
+package com.vanessamatos.vmlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmlearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmlearnApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vanessamatos/vmlearn/VmlearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanessamatos.vmlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmlearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
