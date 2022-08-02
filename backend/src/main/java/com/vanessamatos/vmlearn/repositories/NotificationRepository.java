5befcccf99f29832f512778da9a5710911554279
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-08-02 16:04:05 -0300

    @Query para buscar as notificações

--- backend/src/main/java/com/vanessamatos/vmlearn/repositories/NotificationRepository.java
+++ backend/src/main/java/com/vanessamatos/vmlearn/repositories/NotificationRepository.java
@@ -5,9 +5,14 @@ import com.vanessamatos.vmlearn.entities.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
-    Page<Notification> findByUser(User user, Pageable pageable);
+    @Query("SELECT n FROM Notification n WHERE "
+            + "(n.user = :user) AND "
+            + "(:unreadOnly = false OR n.read = false) "
+            + "ORDER BY n.moment DESC")
+    Page<Notification> find(User user, boolean unreadOnly, Pageable pageable);
 
 }
